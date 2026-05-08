df4cf4101dfcdd2363f69a44f79ac112ac8acbc1
godwinlabaya <glabaya123@gmail.com>
2026-05-09 07:53:22 +0800

    refactor: moved shared folder under src folder

--- web/src/shared/components/Sidebar.js
+++ web/src/shared/components/Sidebar.js
@@ -1,5 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { supabase } from "../../src/supabaseClient";
+import { supabase } from "../../supabaseClient";
 import "./Sidebar.css";
 
 const menuItems = [
